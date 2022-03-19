529c28deea0fe4db73865045632ea4520056494f
antoinemeyer5 <antoine.meyer54@gmail.com>
2022-03-19 19:47:34 +0100

    test pour empiler pile

--- sources/tests.c
+++ sources/tests.c
@@ -8,11 +8,14 @@
 ELEMENT
 */
 
-int test_init_element(elt_t *element, int age, char prenom[])
+int test_init_element()
 {
     int retour = 0;
-    init_element(element, age, prenom);
-    if (element->age == age && strcmp(element->prenom, prenom) == 0)
+    elt_t element_test;
+    int age_test = 22;
+    char prenom_test[] = "Antoine";
+    init_element(&element_test, age_test, prenom_test);
+    if (element_test.age == age_test && strcmp(element_test.prenom, prenom_test) == 0)
     {
         retour = 1;
     }
@@ -23,13 +26,35 @@ int test_init_element(elt_t *element, int age, char prenom[])
 PILE
 */
 
-int test_init_pile(int taille)
+int test_init_pile()
 {
     int retour = 0;
     pile_t pile;
+    int taille = 10;
     pile = init_pile(taille);
-    if(pile.taille == taille && pile.sommet == -1){
+    if (pile.taille == taille && pile.sommet == -1)
+    {
         retour = 1;
+    }
+    return retour;
+}
+
+int test_empiler_pile()
+{
+    elt_t element_test;
+    pile_t pile;
+    int retour = 0;
+    char prenom_test[] = "Antoine";
+    int age_test = 22;
+    pile = init_pile(2);
+    init_element(&element_test, age_test, prenom_test);
+    empiler_pile(&pile, element_test);
+    if (pile.sommet == 0 && pile.tetepile[pile.sommet].age == age_test)
+    {
+        if (strcmp(pile.tetepile[pile.sommet].prenom, prenom_test) == 0)
+        {
+            retour = 1;
+        }
     }
     return retour;
 }
@@ -42,13 +67,16 @@ void tous_les_tests()
 {
     int retour_des_tests = -1;
     elt_t element_de_test;
+    pile_t pile_de_test;
 
     printf("\nPartie de tests : 1 = RAS, 0 = ERR\n");
 
-    retour_des_tests = test_init_pile(10);
+    retour_des_tests = test_init_pile();
     printf("-> init pile : %d\n", retour_des_tests);
 
-    retour_des_tests = test_init_element(&element_de_test, 22, "Antoine");
+    retour_des_tests = test_init_element();
     printf("-> init element : %d\n", retour_des_tests);
 
+    retour_des_tests = test_empiler_pile();
+    printf("-> empiler pile : %d\n", retour_des_tests);
 }
--- sources/tests.h
+++ sources/tests.h
@@ -4,15 +4,15 @@ void tous_les_tests();
 ELEMENT
 */
 
-int test_init_element(elt_t *, int, char[]);
+int test_init_element();
 
 /*
 PILE
 */
 
-int test_init_pile(int);
-/*void empiler_pile(pile_t *, elt_t);
-int est_vide_pile(pile_t);
+int test_init_pile();
+int test_empiler_pile();
+/*int est_vide_pile(pile_t);
 void depiler_pile(pile_t *);
 elt_t depiler_avec_retour_pile(pile_t *);
 elt_t sommet_pile(pile_t);
